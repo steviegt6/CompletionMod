1044a7334b1b049edbef92d5ec9da144f388c199
Stevie <xxlennygamerxx@gmail.com>
2020-03-15 01:07:56 -0700

    Copper/Tin Recipe Group

--- CompletionMod.cs
+++ CompletionMod.cs
@@ -1,9 +1,24 @@
+using Terraria;
+using Terraria.ID;
 using Terraria.ModLoader;
+using Terraria.Localization;
+
 
 namespace CompletionMod
 {
 	public class CompletionMod : Mod
 	{
+		public override void AddRecipeGroups()
+		{
+			base.AddRecipeGroups();
+			RecipeGroup group1 = new RecipeGroup(() => Language.GetTextValue("LegacyMisc.37") + " Copper or Tin Bar", new int[]
+			{
+				ModContent.ItemType<Placeholders.CopperTinBar>(),
+				ItemID.CopperBar,
+				ItemID.TinBar
+			});
+			RecipeGroup.RegisterGroup("CompletionMod:CopperTinBar", group1);
+		}
 		public CompletionMod()
 		{
 		}
