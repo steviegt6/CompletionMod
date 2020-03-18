ee542f91c75067aa9473ed2f843d1df657f55ff8
Stevie <xxlennygamerxx@gmail.com>
2020-03-18 16:22:10 -0700

    Value fixes + Green Candy Cane Sword

--- Items/CandyCane/GreenCandyCaneSword.cs
+++ Items/CandyCane/GreenCandyCaneSword.cs
@@ -4,25 +4,24 @@ using Terraria.ModLoader;
 
 namespace CompletionMod.Items.CandyCane
 {
-    //Misspelling of "Candy" is a reference to the actual Candy Cane Pickaxe. (https://terraria.gamepedia.com/Candy_Cane_Pickaxe#Trivia)
-    public class GreenCnadyCanePickaxe : ModItem
+    public class GreenCandyCaneSword : ModItem
     {
         public override void SetStaticDefaults()
         {
             base.SetStaticDefaults();
             DisplayName.SetDefault("Green Candy Cane Pickaxe");
-            Tooltip.SetDefault("Can mine Meteorite");
+            //Tooltip.SetDefault("");
         }
         public override void SetDefaults()
         {
             base.SetDefaults();
-            item.CloneDefaults(ItemID.CnadyCanePickaxe);
+            item.CloneDefaults(ItemID.CandyCaneSword);
         }
         public override void AddRecipes()
         {
             base.AddRecipes();
             ModRecipe recipe = new ModRecipe(mod);
-            recipe.AddIngredient(ItemID.GreenCandyCaneBlock, 35);
+            recipe.AddIngredient(ItemID.GreenCandyCaneBlock, 50);
             recipe.AddTile(TileID.Anvils);
             recipe.SetResult(this);
             recipe.AddRecipe();
