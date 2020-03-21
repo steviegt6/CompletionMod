6f1f1ef101fb758eb6223b131da26e4ce71426e4
Stevie <xxlennygamerxx@gmail.com>
2020-03-20 19:55:05 -0700

    Anti-Venom Item

---
+++ Items/Accessories/AntiVenom.cs
@@ -0,0 +1,31 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CompletionMod.Items.Accessories
+{
+    [AutoloadEquip(EquipType.Waist)]
+    public class AntiVenom : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            base.SetStaticDefaults();
+            DisplayName.SetDefault("Anti-Venom");
+            Tooltip.SetDefault("Immunity to Venom");
+        }
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+            item.width = 22;
+            item.height = 30;
+            item.value = ((10 * 100) * 5) * 5;
+            item.rare = 4;
+            item.accessory = true;
+        }
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            base.UpdateAccessory(player, hideVisual);
+            player.buffImmune[BuffID.Venom] = true;
+        }
+    }
+}
