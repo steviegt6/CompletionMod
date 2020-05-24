58f3d0604a80eb7160c12e1860309ee2fe4d79f8
Stevie <xxlennygamerxx@gmail.com>
2020-05-23 17:05:27 -0700

    Changed Recipe Group Location

--- HelpersHandlersandUtils/RecipeHelper.cs
+++ HelpersHandlersandUtils/RecipeHelper.cs
@@ -1,5 +1,6 @@
 ﻿using Terraria;
 using Terraria.ID;
+using Terraria.Localization;
 using Terraria.ModLoader;
 using static Terraria.ModLoader.ModContent;
 
@@ -48,6 +49,64 @@ namespace CompletionMod.HelpersHandlersandUtils
 			recipe.AddTile(TileID.Anvils);
 			recipe.SetResult(ItemID.CandyCaneHook);
 			recipe.AddRecipe();
+		}
+		public static void AddCompletionRecipeGroups()
+		{
+			RecipeGroup group1 = new RecipeGroup(() => Language.GetTextValue("LegacyMisc.37") + " Copper or Tin Bar", new int[]
+			{
+				ItemType<Placeholders.CopperTinBar>(),
+				ItemID.CopperBar,
+				ItemID.TinBar
+			});
+			RecipeGroup.RegisterGroup("CompletionMod:CopperTinBar", group1);
+
+			RecipeGroup group2 = new RecipeGroup(() => Language.GetTextValue("LegacyMisc.37") + " Iron or Lead Bar", new int[]
+			{
+				ItemType<Placeholders.IronLeadBar>(),
+				ItemID.IronBar,
+				ItemID.LeadBar
+			});
+			RecipeGroup.RegisterGroup("CompletionMod:IronLeadBar", group2);
+
+			RecipeGroup group3 = new RecipeGroup(() => Language.GetTextValue("LegacyMisc.37") + " Silver or Tungsten Bar", new int[]
+			{
+				ItemType<Placeholders.SilverTungstenBar>(),
+				ItemID.SilverBar,
+				ItemID.TungstenBar
+			});
+			RecipeGroup.RegisterGroup("CompletionMod:SilverTungstenBar", group3);
+
+			RecipeGroup group4 = new RecipeGroup(() => Language.GetTextValue("LegacyMisc.37") + " Gold or Platinum Bar", new int[]
+			{
+				ItemType<Placeholders.GoldPlatinumBar>(),
+				ItemID.GoldBar,
+				ItemID.PlatinumBar
+			});
+			RecipeGroup.RegisterGroup("CompletionMod:GoldPlatinumBar", group4);
+
+			RecipeGroup group5 = new RecipeGroup(() => Language.GetTextValue("LegacyMisc.37") + " Cobalt or Palladium Bar", new int[]
+			{
+				ItemType<Placeholders.CobaltPalladiumBar>(),
+				ItemID.CobaltBar,
+				ItemID.PalladiumBar
+			});
+			RecipeGroup.RegisterGroup("CompletionMod:CobaltPalladiumBar", group5);
+
+			RecipeGroup group6 = new RecipeGroup(() => Language.GetTextValue("LegacyMisc.37") + " Mythril or Orichalcum Bar", new int[]
+			{
+				ItemType<Placeholders.MythrilOrichalcumBar>(),
+				ItemID.MythrilBar,
+				ItemID.OrichalcumBar
+			});
+			RecipeGroup.RegisterGroup("CompletionMod:MythrilOrichalcumBar", group6);
+
+			RecipeGroup group7 = new RecipeGroup(() => Language.GetTextValue("LegacyMisc.37") + " Adamantite or Titanium Bar", new int[]
+			{
+				ItemType<Placeholders.AdamantiteTitaniumBar>(),
+				ItemID.AdamantiteBar,
+				ItemID.TitaniumBar
+			});
+			RecipeGroup.RegisterGroup("CompletionMod:AdamantiteTitaniumBar", group7);
 		}
 		public static void SummonReplacements(Mod mod)
         {
