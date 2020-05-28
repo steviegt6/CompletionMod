17d4587b74a2916217be52cee1ab0854808b496e
Stevie <xxlennygamerxx@gmail.com>
2020-05-27 21:43:21 -0700

    Update CompletionMod.cs

--- CompletionMod.cs
+++ CompletionMod.cs
@@ -20,14 +20,14 @@ namespace CompletionMod
 
 			AddToggle("General", "General", ItemID.Acorn, "6B5D37");
 			AddToggle("AutoConvert", "Auto-Convert Boss Summons", ItemID.SuspiciousLookingEye, "FFFFFF");
-			AddToggle("RecipeSwitch", "Add Extra Recipe Groups", ItemID.SoulofFlight, "42CEE4");
-			AddToggle("PotionsNeeded", "Potion Stack Needed", ItemID.TallyCounter, "89909B");
-			AddToggle("InfPotionsEnabled", "Get Potion Buff when You Have " + Config.Instance.potionsNeeded + "+ Potions", ItemID.RestorationPotion, "F965BD");
-			AddToggle("ThrowingNeeded", "Throwing Item Stack Needed", ItemID.TallyCounter, "89909B");
-			AddToggle("InfThrowingEnabled", "Infinite throwing items when you have " + Config.Instance.throwingNeeded + "+ of an Item", ItemID.StarAnise, "CB8F54");
+			AddToggle("RecipeSwitch", "Add Extra Recipe Groups", ItemID.SoulofFlight, "FFFFFF");
+			AddToggle("PotionsNeeded", "Potion Stack Needed", ItemID.TallyCounter, "FFFFFF");
+			AddToggle("InfPotionsEnabled", "Get Potion Buff when You Have " + Config.Instance.potionsNeeded + "+ Potions", ItemID.RestorationPotion, "FFFFFF");
+			AddToggle("ThrowingNeeded", "Throwing Item Stack Needed", ItemID.TallyCounter, "FFFFFF");
+			AddToggle("InfThrowingEnabled", "Infinite throwing items when you have " + Config.Instance.throwingNeeded + "+ of an Item", ItemID.StarAnise, "FFFFFF");
 
 			AddToggle("Misc", "Miscellaneous", ItemID.SoulofMight, "4BABFF");
-			AddToggle("MaxStack", "Max Stack Increase", ItemID.TallyCounter, "89909B");
+			AddToggle("MaxStack", "Max Stack Increase", ItemID.TallyCounter, "FFFFFF");
 		}
 		public override void Unload()
 		{
